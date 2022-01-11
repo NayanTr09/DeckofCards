b6557ae954ff3080da95d53b17fa407003e09d55
NayanTr09 <nayantripathi42@gmail.com>
2022-01-11 08:18:42 +0530

    Added : Welcome Message

---
+++ src/com/deckofcards/DeckofCards.java
@@ -0,0 +1,12 @@
+/* @Author : Nayan Tripathi
+@Date : 11/01/2021
+Workshop Program
+ */
+
+package com.deckofcards;
+
+public class DeckofCards {
+    public static void main(String args[]){
+        System.out.println("Welcome to Deck of Cards!"); //Welcome message
+    }
+}
